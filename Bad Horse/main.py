0a58d106b4abe850f0e49bd47bac21be2fbf6dca
Nicholas Sidwell <nicholas.sidwell@gmail.com>
2015-09-16 04:22:00 -0500

    Completed Bad Horse.

--- Bad Horse/main.py
+++ Bad Horse/main.py
@@ -1,49 +1,30 @@
+#from https://code.google.com/codejam/contest/6234486/dashboard
+
 PROBLEM = 'A-'
 SMALL = 'small-practice-1'
-LARGE = 'large-practice-2' 
+LARGE = 'small-practice-2' 
 IN = '.in'
 OUT = '.out'
 SIZE = SMALL
 
-def test_member(member, graph, group_a, group_b):
-
-    group_a_intersect = [memb for memb in graph[member] if memb in group_a]
-    group_b_intersect = [memb for memb in graph[member] if memb in group_b]
-    a_intersect_length = len(group_a_intersect)
-    b_intersect_length = len(group_b_intersect)
-    if a_intersect_length == 0:
-        group_a.append(member)
-    else:
-        if b_intersect_length == 0:
-            group_b.append(member)
-        else:
-            print('No')
-    print('Yes')
-
 def can_split(graph):
-    group_a = []
-    group_b = []
-    for member in graph:
-        group_a_intersect = [memb for memb in graph[member] if memb in group_a]
-        group_b_intersect = [memb for memb in graph[member] if memb in group_b]
-        a_intersect_length = len(group_a_intersect)
-        b_intersect_length = len(group_b_intersect)
-        if a_intersect_length == 0:
-            group_a.append(member)
-        else:
-            if b_intersect_length == 0:
-                group_b.append(member)
-            else:
-                for memb in group_b_intersect:
-                    group_b.remove(memb)
-                    test_member(memb, graph, group_a, group_b)
-                
-                    
-                print('No', graph, group_a, group_b)
-                return 'No'
-    print('Yes', graph, group_a, group_b)
-    return 'Yes'
-
+    group_a = set()
+    group_b = set()
+    inserted = set()
+    members = set(graph.keys())
+    while not inserted == members:
+        for member in graph:
+            if len(group_a) == 0:
+                group_a.add(member)
+                inserted.add(member)
+            if member in group_a:
+                inserted.add(member)
+                group_b.update(graph[member])
+            if member in group_b:
+                inserted.add(member)
+                group_a.update(graph[member])
+    return 'Yes' if group_a.isdisjoint(group_b) else 'No'
+                            
 with open(PROBLEM + SIZE + IN, 'r') as f:
     with open(PROBLEM + SIZE + OUT, 'w') as o:
         number_of_cases = int(f.readline())
@@ -52,18 +33,18 @@ with open(PROBLEM + SIZE + IN, 'r') as f:
             current_case = current_case + 1
             number_of_pairs = int(f.readline())
             if number_of_pairs == 1:
-                print('Case #{}: {}\n'.format(current_case, 'Yes'))       
-            graph = {}
-            for i in range(number_of_pairs):
-                a, b = f.readline().rstrip('\n').split(' ')
-                if not a in graph:
-                    graph[a] = [b]
-                else:
-                    graph[a].append(b)
-                if not b in graph:
-                    graph[b] = [a]
-                else:
-                    graph[b].append(a)
-            result = can_split(graph)
-            print('Case #{}: {}\n'.format(current_case, result))
-            o.write('Case #{}: {}\n'.format(current_case, result))
+                f.readline()
+                o.write('Case #{}: {}\n'.format(current_case, 'Yes'))
+            else:
+                graph = {}
+                for i in range(number_of_pairs):
+                    a, b = f.readline().rstrip('\n').split(' ')
+                    if not a in graph:
+                        graph[a] = [b]
+                    else:
+                        graph[a].append(b)
+                    if not b in graph:
+                        graph[b] = [a]
+                    else:
+                        graph[b].append(a)  
+                o.write('Case #{}: {}\n'.format(current_case, can_split(graph)))
